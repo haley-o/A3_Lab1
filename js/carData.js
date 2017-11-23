594e61f764114fd34fc3ef7f13757cd96910ce8d
Haley Ostrander <h_ostrander@outlook.com>
2017-11-22 22:26:23 -0500

    swapped the data and added a fade effect to the car images

--- js/carData.js
+++ js/carData.js
@@ -1,3 +1,34 @@
 var carData = {
   // add info for the cars here
-}
+
+  F55: {
+
+  			modelName : "Blue Car",
+
+  			priceInfo : "$$1000",
+
+			modelDetails : "Synergistically leverage other's extensible total linkage through wireless networks. Appropriately exploit resource maximizing scenarios before wireless outsourcing.",
+
+		},
+
+  F56: {
+
+  			modelName : "Yellow Car",
+
+  			priceInfo : "$$8000",
+
+			modelDetails : "Second leverage other's extensible total linkage through wireless networks. Appropriately exploit resource maximizing scenarios before wireless outsourcing.",
+
+		},
+
+  R58: {
+
+  			modelName : "Red Car",
+
+  			priceInfo : "$$10.00",
+
+			modelDetails : "Third leverage other's extensible total linkage through wireless networks. Appropriately exploit resource maximizing scenarios before wireless outsourcing.",
+
+		}
+
+};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
-   
+  var 	carImage = document.querySelectorAll('.data-ref'),
+  		carTitle = document.querySelector('.modelName'),
+  		carPrice = document.querySelector('.priceInfo'),
+  		carDesc = document.querySelector('.modelDetails');
 
+	function changeCar() {
+
+	 	objectIndex = carData[this.id];
+
+	    carTitle.firstChild.nodeValue = objectIndex.modelName;
+		carPrice.firstChild.nodeValue = objectIndex.priceInfo;
+		carDesc.firstChild.nodeValue = objectIndex.modelDetails;
+
+
+		// if the classList of the element you clicked does not contain the class 'active'
+		// ( ! the exclamation mark reverses the if statment logic, instead of checking if it is true, it checks if it is false)
+		// remove the class active from all items within the carImage array
+		// and add a class of active to the selected item
+		if (!this.classList.contains('active')) {
+			for (var i = 0; i < carImage.length; i++) {
+				carImage[i].classList.remove('active');
+			}
+			this.classList.add('active');
+		}
+  	}
+
+
+	// setting the initial value of i to 0. 
+	// if i is less than the amount of items in the slides array than the function runs once
+	// after the function runs once i incriments by 1
+	// in summary: the loop continues until i is no longer less than the amount of items in the slides array, then it stops.
+	for (var i = 0; i < carImage.length; i++) { 
+	    carImage[i].addEventListener('click', changeCar, false);
+	}
+
+	document.addEventListener("DOMContentLoaded", function(event) { 
+	  carImage[0].click();
+	})
+   
 })();
